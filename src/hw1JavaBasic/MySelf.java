b45d5418011206e67963a1689d2410a78d5c4e96
Rudaina Azam <rudainaazam@Rudainas-MBP.lan>
2023-05-17 20:21:30 -0400

    HW

---
+++ src/hw1JavaBasic/MySelf.java
@@ -0,0 +1,26 @@
+package hw1JavaBasic;
+
+/*
+ * The Naming Convention for Project:
+ * Generally starts with Uppercase then Camel case feature for next word.
+ * Alternative feature for Camel case is called Snake case , generally starts with uppercase and the use _ before the 
+ * second word plus second word will always starts with lowercase.
+ * Camel case example for Project--JavaClass2023
+ * Camel case example for Package--lecNumber01
+ * Camel case example for Class --MainMethod
+ * Alternative example of Camel case, for a project --> Java_class_2023 
+ * Alternative example of Camel case, for a package --> lec_number_01
+ * Alternative example of Camel case, for a class --> Main_method
+ */
+public class MySelf {
+
+	// main method is the entry point of Java.
+	//It is a static method.
+	//Represented by public static void main(String[] args) {}
+
+	public static void main(String[] args) {
+		System.out.println("Roksana Akter");
+		System.out.println("150-05, 88th ave ,Jamaica ");
+
+	}
+}
---
+++ src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,13 @@
+package hw2JavaBasics;
+
+public class MyCar {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Roksana," + "I love to drive," + "My car info is below");
+		System.out.print("\nMy car clolor is white\t");
+		System.out.print("\nMy car model is BMW\t");
+		System.out.print("\nThe year of my car is 2021\t");
+		System.out.print("\nWheel drive of my car 30i\t");
+	}
+
+}
