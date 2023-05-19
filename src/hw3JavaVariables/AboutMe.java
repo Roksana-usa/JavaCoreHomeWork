055b5e1831baa7c28c54338e2f70f34e1e2e4e9e
Roksana-usa <roksanausabd@gmail.com>
2023-05-18 22:19:23 -0400

    homework

---
+++ src/hw3JavaVariables/AboutMe.java
@@ -0,0 +1,24 @@
+package hw3JavaVariables;
+
+public class AboutMe {
+	
+
+		
+	
+//	AboutMe	
+//	public String myName;//Here variable declared.
+	
+	//cmnd + / --> comment
+	//cmnd + c ==> copy
+	//cmnd + v --> paste
+	//cmnd + d --> delete line
+	
+	public String myName;//Here variable declared.
+	public String fullName="Roksana Akter";// Here variable initialized.
+	public byte myAge=60;
+	public char mySEX='F';
+	public int mySalary= 54000;
+	public float myGrade=3.4f;
+	public short myCarPrice=3;
+
+}
